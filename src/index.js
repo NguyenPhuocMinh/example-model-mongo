ed3ae792e1ad2df8bbad5d14e077428b3565c5ef
Minh Nguyen <xiuquay55@gmail.com>
2021-07-27 16:30:17 +0700

    add role model to index

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 
 const UserModel = require('./models/user-model');
 const BoardModel = require('./models/board-model');
+const RoleModel = require('./models/role-model');
 
 const models = [
   UserModel,
-  BoardModel
+  BoardModel,
+  RoleModel
 ];
 
 module.exports = models;
